76a31d78cce7137d82dca78defe3374d3adc1135
Martin <mr.mmaurer+work@gmail.com>
2019-03-29 23:53:13 +0100

    Added mock data

--- scrape-train-data.js
+++ scrape-train-data.js
@@ -1,9 +1,36 @@
+import fs from 'fs-extra';
+
 import Scraper from './src/Scraper/scraper';
 import config from './src/Scraper/scraper.conf';
 
 const scraperFernbahn = new Scraper(config);
 
+const saveMockData = content => new Promise((resolve, reject) => {
+  const newFile = fs.createWriteStream(`${__dirname}/mockData/trains.json`);
+
+  newFile.on('error', () => {
+    reject();
+  });
+
+  newFile.on('finish', () => {
+    resolve('done');
+  });
+
+  newFile.write(content, 'utf8');
+  newFile.end();
+});
+
 scraperFernbahn.scrapeData()
   .then((entries) => {
-    console.log(entries);
+    const jsonEncoded = JSON.stringify(entries, null, 2);
+
+    console.log(jsonEncoded);
+
+    saveMockData(jsonEncoded)
+      .then(() => {
+        process.exit(0);
+      })
+      .catch(() => {
+        process.exit(1);
+      });
   });
--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import fs from 'fs-extra';
+
 import './App.css';
 import SearchForm from './SearchForm/SearchForm';
 import TrainDetails from './TrainDetails/TrainDetails';
@@ -18,6 +20,30 @@ class App extends Component {
     };
 
     this.handleTrainChangeEvent = this.handleTrainChangeEvent.bind(this);
+  }
+
+  fetchMockData() {
+    const file = fs.readFile(`${__dirname}/mockData/trains.json`);
+
+    file
+      .then((entries) => {
+        const parsedJSON = JSON.parse(entries);
+
+        return parsedJSON;
+      })
+      .then((entries) => {
+        const sortedEntries = [...entries].sort((entry1, entry2) => {
+          return entry1.trainNumber - entry2.trainNumber;
+        })
+
+        return sortedEntries
+      })
+      .then((entries) => {
+        // extract only number for now
+        const trainNumbers = entries.map((entry) => entry.trainNumber);
+
+        return(trainNumbers);
+      });
   }
 
   fetchData(){
@@ -45,7 +71,9 @@ class App extends Component {
       isLoading: true,
     });
 
-    this.fetchData()
+    const trainData = (process.env.NODE_ENV === 'dev') ? this.fetchMockData() : this.fetchData();
+
+    trainData
       .then((trains) => {
         this.setState({
           trainNumbers: trains,
