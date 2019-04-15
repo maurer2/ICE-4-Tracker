5e9554b18d4ab354392c94be803ee3d27f60aadc
Martin <mr.mmaurer+work@gmail.com>
2019-04-15 23:31:30 +0200

    Updated train details test and snapshot

--- src/TrainDetails/TrainDetails.test.js
+++ src/TrainDetails/TrainDetails.test.js
@@ -1,19 +1,26 @@
 import React from 'react';
 import TrainDetails from './TrainDetails';
-import Enzyme, { shallow, mount } from 'enzyme';
+import Enzyme, { shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import toJson from 'enzyme-to-json';
 
 Enzyme.configure({ adapter: new Adapter() });
 
 describe('TrainDetails', () => {
+  const dummyTrainDetails = {
+    trainNumber: '502',
+    origin: 'M�nchen Hbf',
+    destination: 'Hamburg-Altona/Hmb Hbf'
+  }
+
   const wrapper = shallow(
     <TrainDetails
-      selectedTrainNumber={'500'}
+      selectedTrainNumber={ '500' }
+      trainDetails={ dummyTrainDetails }
     />
   );
 
-  it('TrainDetails should match snapshot', () => {
+  test('TrainDetails should match snapshot', () => {
     expect(toJson(wrapper)).toMatchSnapshot();
   });
 
